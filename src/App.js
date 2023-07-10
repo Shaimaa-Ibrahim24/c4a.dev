604f0288fadb9a29a6e1efd23717bfbba7ec5cc5
Shaimaa-Ibrahim24 <shimaaebrahem24@gmail.com>
2023-07-11 01:43:15 +0300

    update me

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-    <h1>version two</h1>
+    <h1>version two second</h1>
       <h1 className="title">hello sedra</h1>
       <h2 className="title">hello emy</h2>
     </>
